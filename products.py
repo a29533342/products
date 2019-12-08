7565f8fcea273a8e07d497cda343f53ee5144861
Heather <a29533342@gmail.com>
2019-12-08 16:39:36 +0800

    add write

--- products.py
+++ products.py
@@ -20,4 +20,9 @@ for p in products:
 	print(p)
 	print(p[0], '這個商品的價格是:', p[1], '元')
 
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品名稱, 價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
 
